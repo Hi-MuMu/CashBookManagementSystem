821b247e55a0adc1646c1327d3440a1b63ba3139
USER <USER@PE70-NOTE>
2020-03-22 12:58:44 +0900

    Creat a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("*** Clun Management System Menu ***");
+		System.out.println(" 1. Add Club Member");
+		System.out.println(" 2. Delete Club Member");
+		System.out.println(" 3. Edit Club Member");
+		System.out.println(" 4. View Club Member");
+		System.out.println(" 5. Exit");
+		System.out.println("Select one number between 1 - 5 :");
+	}
+}
