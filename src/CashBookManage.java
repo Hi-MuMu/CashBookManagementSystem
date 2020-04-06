ad8d4f39aeb70ecbf7a7a0dcf0fe4b929a205f68
서무성 <62416298+seomuseong1614@users.noreply.github.com>
2020-04-07 03:16:04 +0900

    Update CashBookManage.java

--- src/CashBookManage.java
+++ src/CashBookManage.java
@@ -45,19 +45,37 @@ public class CashBookManager {
 		String date1 = input.next();
 		for (int i = 0; i< 7;i++) {
 			if(date1.equals(cashbook.date[i])) {
-				System.out.println("수정전 수입 :" + cashbook.incomings[i] + "지출 :" + cashbook.outgoings[i] + "합계 :" + cashbook.total);
-				cashbook.total = cashbook.total - cashbook.incomings[i] + cashbook.outgoings[i];
-				cashbook.sum = cashbook.sum - cashbook.incomings[i];
-				cashbook.Minus = cashbook.Minus - cashbook.outgoings[i];
-				System.out.println("수정할 수입을 입력하세요 : ");
-				cashbook.incomings[i] = input.nextInt();
-				System.out.println("수정할 지출을 입력하세요 : ");
-				cashbook.outgoings[i] = input.nextInt();
-				cashbook.total = cashbook.total + cashbook.incomings[i] - cashbook.outgoings[i];
-				cashbook.sum = cashbook.sum + cashbook.incomings[i];
-				cashbook.Minus = cashbook.Minus + cashbook.outgoings[i];
-				System.out.println("수입과 지출과 합계가 수정됐습니다.");
-				System.out.println("수정후 수입 :" + cashbook.incomings[i] + "지출 :" + cashbook.outgoings[i] + "합계 :" + cashbook.total);
+				int num=4;
+				while (num != 5) {
+					System.out.println("** Cash Book Info Edit Menu **");
+					System.out.println(" 1. Edit Date ");
+					System.out.println(" 2. Editte Cash Book");
+					System.out.println(" 5. Exit");
+					System.out.println("Select one number between 1 - 3 :");
+					num = input.nextInt();
+					if (num==1) {
+						System.out.println("날짜를 입력하세요 :");
+						cashbook.date[i] = input.next();
+					}
+					else if (num==2) {
+						System.out.println("수정전 수입 :" + cashbook.incomings[i] + "지출 :" + cashbook.outgoings[i] + "합계 :" + cashbook.total);
+						cashbook.total = cashbook.total - cashbook.incomings[i] + cashbook.outgoings[i];
+						cashbook.sum = cashbook.sum - cashbook.incomings[i];
+						cashbook.Minus = cashbook.Minus - cashbook.outgoings[i];
+						System.out.println("수정할 수입을 입력하세요 : ");
+						cashbook.incomings[i] = input.nextInt();
+						System.out.println("수정할 지출을 입력하세요 : ");
+						cashbook.outgoings[i] = input.nextInt();
+						cashbook.total = cashbook.total + cashbook.incomings[i] - cashbook.outgoings[i];
+						cashbook.sum = cashbook.sum + cashbook.incomings[i];
+						cashbook.Minus = cashbook.Minus + cashbook.outgoings[i];
+						System.out.println("수입과 지출과 합계가 수정됐습니다.");
+						System.out.println("수정후 수입 :" + cashbook.incomings[i] + "지출 :" + cashbook.outgoings[i] + "합계 :" + cashbook.total);
+					}
+					else {
+						continue;
+					}
+				}
 			}
 		}
 	}
