b01ebc46ed4d7d96c00ca1a63f97dd09d710225b
서무성 <62416298+seomuseong1614@users.noreply.github.com>
2020-04-14 03:48:51 +0900

    Update CashBook.java

--- src/CashBook.java
+++ src/CashBook.java
@@ -1,31 +1,26 @@
 public class CashBook {
-	String[] date = new String[100];
-	int[] incomings = new int[100];
-	int[] outgoings = new int[100];
-	static int total = 0;
-	int sum;
-	int Minus;
+	String date;
+	int in;
+	int out;
+	int total;	
 	
 	public CashBook() {
-		
 	}
 	
-	public CashBook(String date[],int incomings[], int outgoings[], int total) {
-
+	public CashBook(String date) {
 		this.date = date;
-		this.incomings = incomings;
-		this.outgoings = outgoings;
+	}
+	
+	public CashBook(String date, int in, int out, int total) {
+		this.date = date;
+		this.in = in;
+		this.out = out;
 		this.total = total;
 	}
-	
-	public void define() {
-		sum = 0;
-		Minus = 0;
-		total = 0;
-	}
 	public void printInfo() {
-		System.out.println("총수입 :" +sum);
-		System.out.println("총지출 :" +Minus);
-		System.out.println("합계 :" +total);
+		System.out.println("날짜는 :" + date);
+		System.out.println("수입은 :" + in);
+		System.out.println("지출은 :" + out);
+		System.out.println("총합은 :" + total);
 	}
 }
