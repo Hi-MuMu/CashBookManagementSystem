4ebdb4e57089ba4e39bd0c22ea8569f1d62693da
USER <USER@PE70-NOTE>
2020-04-21 10:28:11 +0900

    상속

--- src/CashBookManager.java
+++ src/CashBookManager.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+
+import cashbook.CashBook;
+import cashbook.FebruaryCashBook;
+
 import java.util.ArrayList;
 
 public class CashBookManager {
@@ -9,15 +13,29 @@ public class CashBookManager {
 	}
 	
 	public void addcashbook() {
-		CashBook cashbook = new CashBook();
-		System.out.println("날짜를 입력하세요 :");
-		cashbook.date = input.next();
-		System.out.println("수입을 입력하세요 :");
-		cashbook.in = input.nextInt(); 
-		System.out.println("지출을 입력하세요 :");
-		cashbook.out = input.nextInt();
-		cashbook.total = cashbook.in - cashbook.out;
-		cashbooks.add(cashbook);
+		int kind = 0;
+		CashBook cashbook;
+		while (kind != 1 && kind != 2){
+			System.out.println("1. for January");
+			System.out.println("2. for February");
+			System.out.println("Select num for Month kind between 1 and 2 :");
+			kind = input.nextInt();
+			if (kind == 1) {
+				cashbook = new CashBook();
+				cashbook.getUserInput(input);
+				cashbooks.add(cashbook);
+				break;
+			}
+			else if (kind == 2) {
+				cashbook = new FebruaryCashBook();
+				cashbook.getUserInput(input);
+				cashbooks.add(cashbook);
+				break;
+			}
+			else {
+				System.out.println("Select num for Month kind between 1 and 2 :");
+			}
+		}
 	}
 		
 
@@ -26,14 +44,13 @@ public class CashBookManager {
 		String date1 = input.next();
 		int index = -1 ;
 		for (int i= 0; i<cashbooks.size(); i++) {
-			if(date1.equals(cashbooks.get(i).date)) {
+			if(date1.equals(cashbooks.get(i).getDate())) {
 				index = i; 
 				break;
 			}	
 		}
 		if (index >= 0) {
 			cashbooks.remove(index);
-			CashBook.numCashBookDateRegistered--;
 			System.out.println("The cash book " + date1 + "is deleted");
 		}
 		else {
@@ -47,7 +64,7 @@ public class CashBookManager {
 		String date1 = input.next();
 		for (int i= 0; i<cashbooks.size(); i++) {
 			CashBook cashbook = cashbooks.get(i);
-			if(date1.equals(cashbook.date)) {
+			if(date1.equals(cashbook.getDate())) {
 				int num = 4;
 				while (num != 5) {
 					System.out.println("** Cash Book Info Edit Menu **");
@@ -58,21 +75,30 @@ public class CashBookManager {
 					System.out.println(" 5. Exit");
 					System.out.println("Select one number between 1 - 5 :");
 					num = input.nextInt();
-					if (num==1) {
+					if (num == 1) {
 						System.out.println("날짜를 입력하세요 :");
-						cashbook.date = input.next();
+						String date = input.next();
+						cashbook.setDate(date);
 					}
-					else if (num==2) {
-						cashbook.total = cashbook.total - cashbook.in;
+					else if (num == 2) {
+						int in = cashbook.getIn();
+						int total = cashbook.getTotal();
+						total = total - in;
 						System.out.println("수입을 입력하세요 :");
-						cashbook.in = input.nextInt();
-						cashbook.total = cashbook.total + cashbook.in;
+						in = input.nextInt();
+						cashbook.setIn(in);
+						total = total + in;
+						cashbook.setTotal(total);
 					}
-					else if (num==3) {
-						cashbook.total = cashbook.total + cashbook.out;
+					else if (num == 3) {
+						int out = cashbook.getIn();
+						int total = cashbook.getTotal();
+						total = total + out;
 						System.out.println("지출을 입력하세요 :");
-						cashbook.out = input.nextInt();
-						cashbook.total = cashbook.total - cashbook.out;
+						out = input.nextInt();
+						cashbook.setOut(out);
+						total = total - out;
+						cashbook.setTotal(total);
 					}
 					else {
 						continue;
@@ -86,7 +112,7 @@ public class CashBookManager {
 	public void viewcashbooks() {
 //		System.out.println("날짜를 입력하세요 : ");
 //		String date1 = input.next();
-		System.out.println("# of registerd cash book date :"+ CashBook.numCashBookDateRegistered);
+		System.out.println("# of registerd cash book date :"+ cashbooks.size());
 		for (int i= 0; i<cashbooks.size(); i++) {
 			cashbooks.get(i).printInfo();
 			
@@ -94,3 +120,4 @@ public class CashBookManager {
 
 	}
 }
+
