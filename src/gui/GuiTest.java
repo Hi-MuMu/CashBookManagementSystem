c1f2ed42137f9cef108d6d309bd23b4a216b303f
USER <USER@PE70-NOTE>
2020-06-01 13:12:42 +0900

    gui2

--- src/gui/GuiTest.java
+++ src/gui/GuiTest.java
@@ -3,8 +3,8 @@ package gui;
 public class GuiTest {
 
 	public static void main(String[] args) {
-		//MenuSelection menuslection = new MenuSelection();
-		//CashBookAdder cashbookadder = new CashBookAdder();
+		MenuSelection menuslection = new MenuSelection();
+		CashBookAdder cashbookadder = new CashBookAdder();
 		CashBookViewer cashbookviewer = new CashBookViewer();
 	}
 
